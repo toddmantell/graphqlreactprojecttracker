fbcf3d85d4229034c197cc7f58a91fe7fcf0bb5a
Todd Mantell <toddmantell@gmail.com>
2017-08-20 22:47:20 -0500

    added load screen on app load

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,15 +1,32 @@
 import {h, Component} from 'preact';
+import {Loading} from '../loading/Loading';
 
 export default class App extends Component {
   constructor() {
     super();
+
+    this.state = {
+      loading: true
+    };
+
+    this.renderLoadingOrApp = this.renderLoadingOrApp.bind(this);
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({loading: false});
+    }, 750);
+  }
+
+  renderLoadingOrApp() {
+      return this.state.loading ? <Loading /> : (
+        <div>
+          Application Root
+        </div>
+      );
   }
 
   render() {
-    return (
-      <div>
-        Application Root
-      </div>
-    );
+    return this.renderLoadingOrApp();
   }
 }
